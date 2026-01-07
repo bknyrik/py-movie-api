94d5c3c09fda29b079536413db90ef04b618a476
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-01-07 11:42:26 +0200

    Solution 2.0

--- cinema/migrations/0001_initial.py
+++ cinema/migrations/0001_initial.py
@@ -12,12 +12,12 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Movie',
+            name="Movie",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('description', models.CharField(max_length=255, null=True)),
-                ('duration', models.TimeField()),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("title", models.CharField(max_length=255)),
+                ("description", models.CharField(max_length=255, null=True)),
+                ("duration", models.TimeField()),
             ],
         ),
     ]
--- cinema/migrations/0002_alter_movie_duration.py
+++ cinema/migrations/0002_alter_movie_duration.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cinema', '0001_initial'),
+        ("cinema", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='movie',
-            name='duration',
+            model_name="movie",
+            name="duration",
             field=models.IntegerField(),
         ),
     ]
